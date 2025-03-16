92f0755f0e340956704e5b299491dd4259640c59
Taiyue Liu <t67liu@gmail.com>
2025-03-16 11:41:21 -0700

    Initial Commit

---
+++ PostCardIt/PostCardItApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostCardItApp.swift
+//  PostCardIt
+//
+//  Created by Taiyue Liu on 3/15/25.
+//
+
+import SwiftUI
+
+@main
+struct PostCardItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PostCardItTests/PostCardItTests.swift
@@ -0,0 +1,17 @@
+//
+//  PostCardItTests.swift
+//  PostCardItTests
+//
+//  Created by Taiyue Liu on 3/15/25.
+//
+
+import Testing
+@testable import PostCardIt
+
+struct PostCardItTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
